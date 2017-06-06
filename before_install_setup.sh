26104fed36aff088a7904a36370236db50b7458e
alokadhao20 <alokadhao@gmail.com>
2017-06-07 01:34:50 +0530

    added travis

--- before_install_setup.sh
+++ before_install_setup.sh
@@ -2,6 +2,7 @@
 setupNpmrc () {
     echo "setting up .netrc file echo-------"
     echo $TRAVIS_COMMIT
+    echo $GIT_USER_TOKEN
     export COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 }
 
