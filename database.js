8433026a9f3ad690fe4c9f8781bf86976ca6f9b5
alokadhao20 <alokadhao@gmail.com>
2017-06-08 00:44:35 +0530

    updated db connection string

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ function db() {
 db.prototype.connect = function (callback) {
     console.log("I am in db.prototype.connect");
     var MongoClient = require('mongodb').MongoClient;
-    var url = 'mongodb://localhost:27017/mongoMock02';
+    //var url = 'mongodb://localhost:27017/mongoMock02';
+    var url = 'mongodb://alokadhao:noguess21@ds115411.mlab.com:15411/graspu';
     // Connect using MongoClient
     MongoClient.connect(url, function (err, db) {
         if(err) {
